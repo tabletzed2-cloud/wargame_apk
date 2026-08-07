45316d6f6401e4c0f2c9c446d25bb81757d45843
tabletzed2-cloud <tabletzed2@gmail.com>
2026-08-07 12:09:48 +0500

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,12 +1,22 @@
-const CACHE_NAME = 'wargame-v12.16';
+const CACHE_NAME = 'wargame-v12.16'; 
+
 const ASSETS = [
   '/',
   '/index.html',
-  '/manifest.json',
-  '/icon-192.png',
-  '/icon-512.png'
-  // '/images/commisar AIRF.png',
-  // '/images/Горные охотники.png',
+  '/manifest.json'
+  // '/images/terrain/grass.png',  
+  // '/images/terrain/forest.png',
+  // '/images/terrain/road.png',
+  // '/images/terrain/hill.png',
+  // '/images/terrain/water.png',
+  // '/images/terrain/swamp_passable.png',
+  // '/images/terrain/swamp_impassable.png',
+  // '/images/terrain/bushes.png',
+  // '/images/terrain/rocks.png',
+  // '/images/terrain/trenches.png',
+  // '/images/terrain/hill_slope_1.png',
+  // '/images/terrain/hill_slope_2.png',
+  // '/images/terrain/hill_slope_3.png'
 ];
 
 self.addEventListener('install', (event) => {
